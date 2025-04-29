5922b228de8b2db4b9075129e381b93ef684ecf9
Lior Lieberman <liorlieberman@google.com>
2025-04-29 09:45:37 -0700

    change to extproc requestBody processing mode to FULL_DUPLEX_STREAMED for Gateway Inference Extension (#56089)
    * fix: change to FULL_DUPLEX_STREAMED processing mode for GIE
    
    * Automator: update common-files@master in istio/istio@master (#55878)
    
    * TMP: Regen due to odd something version update something somewhere for some reason
    
    * a
    
    * Automator: update proxy@master in istio/istio@master (#56094)
    
    * Create ambient multinetwork flag (#55991)
    
    * Create new multinetwork flag
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Add new topology
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Pass Values.global.network to ztunnel
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Skip workload to workload test if workloads are in different clusters
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Fix several tests that aren't ready for multicluster
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Fix some more tests
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Fix all non-CNI/taint ambient tests
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    * Fix lint
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    ---------
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    
    ---------
    
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>
    Co-authored-by: Aslak Knutsen <aslak@4fs.no>
    Co-authored-by: Istio Automation <istio-testing-bot@google.com>
    Co-authored-by: Keith Mattix II <keithmattix@microsoft.com>

--- pilot/pkg/networking/core/route/route.go
+++ pilot/pkg/networking/core/route/route.go
@@ -522,11 +522,11 @@ func TranslateRoute(
 								ClusterName: model.BuildSubsetKey(model.TrafficDirectionOutbound, "", host.Name(extSvc), int(p)),
 							},
 						},
-					},
-					ProcessingMode: &extproc.ProcessingMode{
-						RequestHeaderMode: extproc.ProcessingMode_SEND,
-						// open AI standard includes the model and other information the ext_proc server needs in the request body
-						RequestBodyMode: extproc.ProcessingMode_BUFFERED,
+						ProcessingMode: &extproc.ProcessingMode{
+							RequestHeaderMode: extproc.ProcessingMode_SEND,
+							// open AI standard includes the model and other information the ext_proc server needs in the request body
+							RequestBodyMode: extproc.ProcessingMode_FULL_DUPLEX_STREAMED,
+						},
 					},
 				},
 			},
